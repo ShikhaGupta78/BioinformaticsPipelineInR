246a0a1ae02665eb6b75433670c0cb99db924a73
ShikhaGupta78 <56894483+ShikhaGupta78@users.noreply.github.com>
2020-11-30 20:17:30 +0100

    Update SRAtoBED.sh

--- SRAtoBED.sh
+++ SRAtoBED.sh
@@ -1,10 +1,10 @@
 # Creation of FastQ file from SRA file, first parameter is the path of the fastq-dump and the second parameter is the location of the SRA file
 /home/tools/sratoolkit.2.5.1-centos_linux64/bin/fastq-dump.2.5.1 /PathToSRA/FolderName/SRA/$1
 
-# BWA command to map the generated FastQ file to the reference genome and index SAI file generated as output
+# BWA command to map the generated FastQ file to the reference genome, and index SAI file is generated as the output
 bwa aln -t 8 /media/lpmb3/iGenomes/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa /media/lpmb3/Shikha/SRA/$1.fastq > $1.sai
 
-# BWA command to finally generate the SAM file out of the index SAI and FastQ files
+# BWA command to finally generate the SAM file from the index SAI and FastQ files
 bwa samse /media/lpmb3/iGenomes/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa $1.sai $1.fastq > $1.aln.sam
 
 # SAMtools command to convert SAM file format to binary version BAM file format
@@ -31,16 +31,4 @@ bedtools merge -i ALL_total_peaks_sorted.bed > ALL_total_peaks_sorted_merged.bed
 # bedtools multicov command
 bedtools multicov -q 20 -bams SRR5445252_sorted.bam SRR5445251_sorted.bam SRR5445213_sorted.bam SRR5445212_sorted.bam -bed /media/lpmb3/Shikha/Q/testShikhaFinalest.bed >
 /media/lpmb3/Shikha/FolderName/final_BED.txt & disown
-
-
-
-
-
-
-
-
-
-
-
-# Generate index out of sorted BAM file from which duplicates were removed
 
