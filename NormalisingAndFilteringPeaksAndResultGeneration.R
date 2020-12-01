0c6931a471817da82eaad54b54f269e970cc817c
ShikhaGupta78 <56894483+ShikhaGupta78@users.noreply.github.com>
2020-12-01 21:31:29 +0100

    Update NormalisingAndFilteringPeaksAndResultGeneration.R

--- NormalisingAndFilteringPeaksAndResultGeneration.R
+++ NormalisingAndFilteringPeaksAndResultGeneration.R
@@ -1,9 +1,9 @@
 setwd("/pathToBedFile/BedFile")
 
-# Read BED file as a data frame
+# Read the BED file as a data frame
 bed <- read.table("final_BED.txt",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
 
-# Create new data frame to store accessible chromosome coordinates, and ATAC and ChIP-seq peak heights 
+# Creating a new data frame to store accessible chromosome coordinates, and ATAC and ChIP-seq peak heights 
 newBedMine <- data.frame(peaks=integer(n), startPeakPos=integer(n), endPeakPos=integer(n), a1Patac=integer(n), a1Natac=integer(n), chip1=integer(n), chip2=integer(n));
 
 # Eliminating rows from initial data frame where peak height (number of reads) of either ATAC-seq Ascl1 induced or uninduced are less than or equal to 0, and 
@@ -13,7 +13,7 @@ newBedMine <- bed[bed$V4>0&bed$V5>0,]
 # Visualizing the new data frame
 newBedMine
 
-# Following steps are to normalize the total number of reads in the two columns "ATAC-seq peaks with Ascl1 induction" and "ATAC-seq peaks without Ascl1 induction"
+# Following steps were to normalize the total number of reads in the two columns "ATAC-seq peaks with Ascl1 induction" and "ATAC-seq peaks without Ascl1 induction"
 
 # Determining the sum of peak heights (sum of number of reads) for "ATAC-seq peaks with Ascl1 induction", for each accessible chromatin region
 a1PatacSum<- sum(newBedMine$V4)
